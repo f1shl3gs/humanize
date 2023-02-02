3f01e389b50f40607b152182beb2935286a594b2
f1shl3gs <fishlegs.engerman@gmail.com>
2023-02-03 01:31:26 +0800

    re-export ByteSize and Duration

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
 #[cfg(feature = "bytes")]
 pub mod bytes;
+#[cfg(feature = "bytes")]
+pub use bytes::ByteSize;
 
 #[cfg(feature = "duration")]
 pub mod duration;
+#[cfg(feature = "duration")]
+pub use duration::Duration;
+

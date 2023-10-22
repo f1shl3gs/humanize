57b184f3c1d8fd1b5c38cc380b58bb56b7944859
f1shl3gs <fishlegs.engerman@gmail.com>
2023-10-23 00:36:06 +0800

    remove space for humanized string

--- src/bytes.rs
+++ src/bytes.rs
@@ -48,7 +48,7 @@ impl From<ParseFloatError> for ParseError {
 ///
 /// See also: `parse_bytes`
 ///
-/// Bytes(82854982) -> 83 MB
+/// bytes(82854982) -> 83 MB
 #[must_use]
 pub fn bytes(s: usize) -> String {
     humanate_bytes(s, 1000.0, ["B", "kB", "MB", "GB", "TB", "PB", "EB"])
@@ -56,7 +56,7 @@ pub fn bytes(s: usize) -> String {
 
 /// ibytes produces a human readable representation of an IEC size.
 ///
-/// IBytes(82854982) -> 79 MiB
+/// ibytes((82854982) -> 79 MiB
 #[must_use]
 pub fn ibytes(s: usize) -> String {
     humanate_bytes(s, 1024.0, ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB"])
@@ -127,16 +127,22 @@ fn logn(n: f64, b: f64) -> f64 {
 
 fn humanate_bytes(s: usize, base: f64, sizes: [&str; 7]) -> String {
     if s < 10 {
-        return format!("{} B", s);
+        return format!("{}B", s);
     }
 
-    let e = logn(s as f64, base);
-    let e = e.floor();
+    let e = logn(s as f64, base).floor();
     let suffix = sizes[e as usize];
     let val = s as f64 / base.powf(e) * 10.0 + 0.5;
     let val = val.floor() / 10.0;
 
-    format!("{} {}", val, suffix)
+    format!("{}{}", val, suffix)
+    /*
+        if val < 10.0 {
+            format!("{:.1}{}", val, suffix)
+        } else {
+            format!("{:.0}{}", val, suffix)
+        }
+    */
 }
 
 #[cfg(feature = "serde")]
@@ -190,21 +196,104 @@ mod tests {
     #[test]
     fn test_parse_bytes() {
         let tests = [
-            ("1k", 1000),
-            ("1ki", 1024),
+            ("42", 42),
             ("42MB", 42000000),
-            ("128MB", 128 * 1000 * 1000),
-            ("128M", 128 * 1000 * 1000),
-            ("128Mi", 128 * 1024 * 1024),
-            ("128mi", 128 * 1024 * 1024),
-            ("128.0MB", 128 * 1000 * 1000),
-            ("128.0m", 128 * 1000 * 1000),
-            ("128.0 MB", 128 * 1000 * 1000),
+            ("42MiB", 44040192),
+            ("42mb", 42000000),
+            ("42mib", 44040192),
+            ("42MIB", 44040192),
+            ("42 MB", 42000000),
+            ("42 MiB", 44040192),
+            ("42 mb", 42000000),
+            ("42 mib", 44040192),
+            ("42 MIB", 44040192),
+            ("42.5MB", 42500000),
+            ("42.5MiB", 44564480),
+            ("42.5 MB", 42500000),
+            ("42.5 MiB", 44564480),
+            // No need to say B
+            ("42M", 42000000),
+            ("42Mi", 44040192),
+            ("42m", 42000000),
+            ("42mi", 44040192),
+            ("42MI", 44040192),
+            ("42 M", 42000000),
+            ("42 Mi", 44040192),
+            ("42 m", 42000000),
+            ("42 mi", 44040192),
+            ("42 MI", 44040192),
+            ("42.5M", 42500000),
+            ("42.5Mi", 44564480),
+            ("42.5 M", 42500000),
+            ("42.5 Mi", 44564480),
+            // Bug #42
+            ("1,005.03 MB", 1005030000),
+            // Large testing, breaks when too much larger than
+            // this.
+            ("12.5 EB", (12.5 * EBYTE as f64) as usize),
+            ("12.5 E", (12.5 * EBYTE as f64) as usize),
+            ("12.5 EiB", (12.5 * EIBYTE as f64) as usize),
         ];
 
         for (input, want) in tests {
             let value = parse_bytes(input).unwrap();
             assert_eq!(value, want as usize, "input: {}", input);
+        }
+    }
+
+    #[test]
+    fn test_bytes() {
+        let tests = [
+            ("bytes(0)", bytes(0), "0B"),
+            ("bytes(1)", bytes(1), "1B"),
+            ("bytes(803)", bytes(803), "803B"),
+            ("bytes(999)", bytes(999), "999B"),
+            ("bytes(1024)", bytes(1024), "1kB"),
+            ("bytes(9999)", bytes(9999), "10kB"),
+            ("bytes(1MB - 1)", bytes(MBYTE - BYTE), "1000kB"),
+            ("bytes(1MB)", bytes(1024 * 1024), "1MB"),
+            ("bytes(1GB - 1K)", bytes(GBYTE - KBYTE), "1000MB"),
+            ("bytes(1GB)", bytes(GBYTE), "1GB"),
+            ("bytes(1TB - 1M)", bytes(TBYTE - MBYTE), "1000GB"),
+            ("bytes(10MB)", bytes(9999 * 1000), "10MB"),
+            ("bytes(1TB)", bytes(TBYTE), "1TB"),
+            ("bytes(1PB - 1T)", bytes(PBYTE - TBYTE), "999TB"),
+            ("bytes(1PB)", bytes(PBYTE), "1PB"),
+            ("bytes(1PB - 1T)", bytes(EBYTE - PBYTE), "999PB"),
+            ("bytes(1EB)", bytes(EBYTE), "1EB"),
+            // Overflows.
+            // ("bytes(1EB - 1P)", Bytes((KBYTE*EBYTE)-PBYTE), "1023EB"),
+            ("bytes(0)", ibytes(0), "0B"),
+            ("bytes(1)", ibytes(1), "1B"),
+            ("bytes(803)", ibytes(803), "803B"),
+            ("bytes(1023)", ibytes(1023), "1023B"),
+            ("bytes(1024)", ibytes(1024), "1KiB"),
+            ("bytes(1MB - 1)", ibytes(MIBYTE - IBYTE), "1024KiB"),
+            ("bytes(1MB)", ibytes(1024 * 1024), "1MiB"),
+            ("bytes(1GB - 1K)", ibytes(GIBYTE - KIBYTE), "1024MiB"),
+            ("bytes(1GB)", ibytes(GIBYTE), "1GiB"),
+            ("bytes(1TB - 1M)", ibytes(TIBYTE - MIBYTE), "1024GiB"),
+            ("bytes(1TB)", ibytes(TIBYTE), "1TiB"),
+            ("bytes(1PB - 1T)", ibytes(PIBYTE - TIBYTE), "1023TiB"),
+            ("bytes(1PB)", ibytes(PIBYTE), "1PiB"),
+            ("bytes(1PB - 1T)", ibytes(EIBYTE - PIBYTE), "1023PiB"),
+            ("bytes(1EiB)", ibytes(EIBYTE), "1EiB"),
+            // Overflows.
+            // ("bytes(1EB - 1P)", ibytes(((KIBYTE*EIBYTE)-PIBYTE), "1023EB"),
+            (
+                "bytes(5.5GiB)",
+                ibytes((5.5 * GIBYTE as f64) as usize),
+                "5.5GiB",
+            ),
+            (
+                "bytes(5.5GB)",
+                bytes((5.5 * GBYTE as f64) as usize),
+                "5.5GB",
+            ),
+        ];
+
+        for (name, got, want) in tests {
+            assert_eq!(got, want, "want in {name:?}, got {got}, want {want}");
         }
     }
 }
